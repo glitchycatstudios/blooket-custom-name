236b85f3d864fb6f2021fa57ae5b9f868bc11ef2
Glitchy Cat Studios <valentrein2011@gmail.com>
2025-01-13 15:35:05 +0100

    fixed security

--- runscript.js
+++ runscript.js
@@ -1,11 +1,20 @@
+function getTabId(callback) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      var tab = tabs[0];
+      callback(tab.id);
+  });
+}
 
-      const chrome = "https://developer.chrome.com/docs/extensions/reference/api/scripting#permissions"
-      function getTabId() { tabId }
-
-        chrome.scripting
-            .executeScript({
-                 target : {tabId : getTabId()},
-                 files : [ "script.js" ],
-             })
-        .then(() => console.log("injected script file"));
-         
+getTabId(function(tabId) {
+  fetch(chrome.runtime.getURL('script.js'))
+      .then(response => response.text())
+      .then(scriptContent => {
+          chrome.scripting.executeScript({
+              target: { tabId: tabId },
+              func: (script) => {
+                  eval(script);
+              },
+              args: [scriptContent]
+          }).then(() => console.log("Injected script content into the console"));
+      });
+});
