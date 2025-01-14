2a0fbe4bbd38667b1643ff9ed0f02c336d5a6b8a
Glitchy Cat Studios <valentrein2011@gmail.com>
2025-01-14 17:10:36 +0100

    almost done

--- popup.js
+++ popup.js
@@ -1,5 +1,20 @@
-document.getElementById('copyButton').addEventListener('click', () => {
-  fetch(chrome.runtime.getURL('codeToCopy.js'))
+document.getElementById('copyButton1').addEventListener('click', () => {
+  fetch(chrome.runtime.getURL('codeToCopy1.js'))
+    .then(response => response.text())
+    .then(textToCopy => {
+      navigator.clipboard.writeText(textToCopy).then(() => {
+        alert('Text copied to clipboard');
+      }).catch(err => {
+        console.error('Failed to copy text: ', err);
+      });
+    })
+    .catch(err => {
+      console.error('Failed to fetch code: ', err);
+    });
+});
+
+document.getElementById('copyButton2').addEventListener('click', () => {
+  fetch(chrome.runtime.getURL('codeToCopy2.js'))
     .then(response => response.text())
     .then(textToCopy => {
       navigator.clipboard.writeText(textToCopy).then(() => {
